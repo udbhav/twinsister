af7d5377cac04d01ac3b8c3fde2d2c495a7487aa
udbhav <udbhav@alyosha.(none)>
2009-11-11 18:16:01 -0500

    upcoming shows

--- src/apps/events/templatetags/shows.py
+++ src/apps/events/templatetags/shows.py
@@ -10,3 +10,10 @@ register = template.Library()
 def shows():
     shows = Show.objects.filter(show_date__gte=datetime.now()).order_by('show_date')
     return {'shows':shows}
+
+@register.filter
+def upcoming_show(show):
+    if show.show_date > datetime.now():
+        return True
+    else:
+        return False
