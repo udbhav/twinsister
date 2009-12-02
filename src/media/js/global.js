7069b5dee5cfdd2c5337052c15a1ac5b31f01129
udbhav <udbhav@alyosha.(none)>
2009-12-02 14:09:22 -0500

    track song plays and downloads

--- src/media/js/global.js
+++ src/media/js/global.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
         link: '.next_page',
         content: '#autopager_content'
     });
+
+    // Track Song plays on Google Analytics
+    $(".tracklist a").click(function() {
+        _trackEvent("Songs", "Play", $(this).html());
+    });
+
+    // Track Release downloads on Google Analytics
+    $(".download_links a").click(function() {
+        var label = $(this).attr("title") + ' ' + $(this).html();
+        _trackEvent("Releases", "Download", label);
 });
