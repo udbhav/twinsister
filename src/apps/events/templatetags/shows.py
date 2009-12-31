7fecb6ebf5e6586cdd1a3e15ffdecf82944118d2
Udbhav Gupta <borges@borges.local>
2009-12-30 23:24:34 -0500

    release thumbnail

--- src/apps/events/templatetags/shows.py
+++ src/apps/events/templatetags/shows.py
@@ -11,6 +11,14 @@ def shows():
     shows = Show.objects.filter(show_date__gte=datetime.now()).order_by('show_date')[:5]
     return {'shows':shows}
 
+@register.inclusion_tag('events/next_show.html')
+def next_show():
+    try:
+        show = Show.objects.filter(show_date__gte=datetime.now()).order_by('show_date')[0]
+    except IndexError:
+        show = None
+    return {'show':show}
+
 @register.filter
 def upcoming_show(show):
     if show.show_date > datetime.now():
