44a3b468366c56edbe4f801b89216adfe6cce4f4
Udbhav <udbhav@twinsister.(none)>
2010-01-04 18:26:14 +0000

    fixed splash screen

--- src/media/js/home.js
+++ src/media/js/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("body").mousemove(function() {
         if ($(".release_info").css("opacity") == 0) {
-            $(".release_info, h3, #more_link").animate({"opacity": "100"}, 10000);
+            $(".release_info, h3, #more_link").animate({"opacity": "100"}, 7000);
         }
     });
 });
