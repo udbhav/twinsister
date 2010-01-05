92917020da21af831f24240d2b09d33fa2d2fb5d
Udbhav <udbhav@twinsister.(none)>
2010-01-05 22:10:31 +0000

    added ep link

--- src/apps/music/templatetags/music.py
+++ src/apps/music/templatetags/music.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.filter
 def get_tracklist(release):
     return release.songs.order_by('track_number')
+
+@register.inclusion_tag('music/release_thumbnails.html')
+def release_thumbnails():
+    releases = Release.objects.filter(official=True).order_by('-pub_date')
+    return {'releases': releases}
