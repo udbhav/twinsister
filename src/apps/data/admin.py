98f23ef2cf0e49aec64c15ad6a3079ff78c18d2e
Udbhav Gupta <udbhav@borges.local>
2010-04-28 11:07:39 -0400

    added published admin action

--- src/apps/data/admin.py
+++ src/apps/data/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from apps.data.models import *
 
+def make_published(modeladmin, request, queryset):
+    queryset.update(published=True)
+make_published.short_description = "Mark selected as published"
+
 class WYMEditorAdmin(admin.ModelAdmin):
         class Media:
                 css = {
@@ -17,6 +21,7 @@ class PrepopulatedAdmin(WYMEditorAdmin):
         prepopulated_fields = {'slug': ('name',)}
         list_display = ('name', 'pub_date', 'published')
         search_fields = ('name',)
+        actions = [make_published]
 
 admin.site.register(Data, PrepopulatedAdmin)
 admin.site.register(HeaderImage)
--- src/apps/events/admin.py
+++ src/apps/events/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 from apps.events.models import Show, Venue, Tour
-from apps.music.admin import PrepopulatedAdmin
+from apps.data.admin import PrepopulatedAdmin, make_published
 
 class ShowAdmin(PrepopulatedAdmin):
     list_display = ('name', 'show_date')
     filter_horizontal = ('bands',)
+    actions = [make_published]
 
 admin.site.register(Show, ShowAdmin)
 admin.site.register(Venue)
